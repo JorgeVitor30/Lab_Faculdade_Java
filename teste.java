9c5a350f790272afa0e062e282a576862c3c8a5d
Jorge Vitor <103287884+JorgeVitor30@users.noreply.github.com>
2023-02-02 22:32:13 -0300

    Introdução ao Java

---
+++ teste.java
@@ -0,0 +1,11 @@
+class PrimeiroTeste {
+    public static void main(String[] args) {
+        sum(4,3);
+    }
+    
+    static void sum(int x, int y){
+        System.out.println(x + y);
+    }
+    
+}
+
